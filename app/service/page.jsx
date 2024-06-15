18b5431ca10d3a4adafad3c06087d3b9c752f114
PyAde <liodeo23@gmail.com>
2024-06-16 02:23:40 +0800

    new

--- app/service/page.jsx
+++ app/service/page.jsx
@@ -36,13 +36,26 @@ const Services = () => {
 	return (
 		<section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-8">
 			<div className="container mx auto">
-				Service
 				<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1,
 					transition: {delay: 2.5, duration: 0.5, ease: "easeIn"}
-				}}>
-					{services.map((service, index) =>{
+				}} className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
+					{services.map((service,index)=>{
 						return <div key={index}>
-							
+							{/*  */}
+							<div>
+								<div>
+									{service.num}
+									<Link href={service.href}>
+									<BsArrowDownRight/>
+									</Link>
+								</div>
+								{/* judul */}
+								<h2>{service.title}</h2>
+								{/* deksripsi */}
+								<p>{service.description}</p>
+								{/* border */}
+								<div className="border-b border-white/20 w-full"></div>
+							</div>
 						</div>
 					})}
 				</motion.div>
